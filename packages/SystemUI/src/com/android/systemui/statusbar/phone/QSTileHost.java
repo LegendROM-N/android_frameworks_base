a32402d23721b4b1b9c53dc36104281535a9c842
Pranav Vashi <neobuddy89@gmail.com>
2016-12-21 18:49:24 +0530

    QS: Add live display tile
    * This is rewrite of old live display tile by CM for 7.1
    * Revert when dynamic tiles are ready by CM.
    
    Change-Id: I941e6787021aeda3e5285a11738bfce120879a98
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.GoogleVoiceAssistTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -486,6 +487,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("voiceassist")) return new GoogleVoiceAssistTile(this);
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
 	else if (tileSpec.equals("lte")) return new LteTile(this);
+	else if (tileSpec.equals("live_display")) return new LiveDisplayTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
