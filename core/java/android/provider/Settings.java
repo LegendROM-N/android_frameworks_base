48cac42440b35981bc59c4a55b228506736a71b7
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-04-11 04:56:08 -0400

    Dialer: show headsup on incoming call even if headsup are off [1/2]
    Old behavior: dialer went full screen when getting a call if heads up
    were off
    
    New behaviors:
    1) you are not in dialer and get a call: system shows headsup thus doesn't change
    the foreground app you were on
    2) you are in dialer and get a call: dialer shows incallui without headsup
    3) you are in dialer and call someone: dialer shows incallui without headsup
    
    Change-Id: I2a750d357c20c112f6d5cd8fab87784a2e53f7b9
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4087,6 +4087,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_TICKER_FONT_SIZE = "status_bar_ticker_font_size";
 
+	/**
+         * Whether to show the heads up for dialer when heads up are disabled
+         * @hide
+         */
+        public static final String ALWAYS_HEADSUP_DIALER = "always_headsup_dialer";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
