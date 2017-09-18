4e9416728819de5faa8d4c26976d201735361ba5
Yashraj Jhala <jhalayashraj@gmail.com>
2017-09-18 18:37:24 +0000

    ColorEngine: default to pixel color accent
    Change-Id: I7e896693b4ce5651f06aaed67a9964347833c8c9

--- services/core/java/com/android/server/ThemeService.java
+++ services/core/java/com/android/server/ThemeService.java
@@ -1,3 +1,4 @@
+
 /**
  * Copyright (C) 2016-2017 The ParanoidAndroid Project
  *
@@ -150,7 +151,7 @@ public class ThemeService extends IThemeService.Stub implements IBinder.DeathRec
 
     private int getAccentColor() {
         return Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.THEME_ACCENT_COLOR, 1);
+                Settings.Secure.THEME_ACCENT_COLOR, 3);
     }
 
     public static void returnToDefaultTheme(Context context) {
